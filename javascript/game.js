92259e008a94bbb55b87cbb74d08d8bd38932380
jenenneihart <neihart.jenen@gmail.com>
2020-02-19 22:44:57 -0700

    final commit

---
+++ javascript/game.js
@@ -0,0 +1,13 @@
+(document).ready(function() {
+
+    var possibleWords = ["florence", "paris", "finland", "madrid", "rome", "dubai", "mexico",]
+function checkForLetter(letter) {
+        var foundLetter = false
+for (var i=0, j= wordToMatch.length; i<j; i++) {
+        if (letter === wordToMatch[i]) {
+            guessingWord[i] = letter
+            foundLetter = true
+    
+function resetGame() {
+                numGuess = maxGuess
+                pauseGame = false
